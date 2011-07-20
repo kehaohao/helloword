46d780a472cde4d9913d5d1380b1bce6b96f7742
kehaohao <534529319@qq.com>
2011-07-20 15:27:52 +0800

    hello

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argv,const *arc[])
+{
+     int i  ;
+     printf("ke hao note\n");
+
+     for(i = 0; i <= 100 ; i ++)
+     { 
+          printf("i is %d\n",i);
+     } 
+     return 0;
+}
