9056a8a06d4f49b7832b3b5941fd00d55b35287f
kehaohao <534529319@qq.com>
2011-07-23 15:54:53 +0800

    ggggggggggggggg

--- hello.c
+++ hello.c
@@ -1,17 +1,25 @@
 #include<stdio.h>
 
 #define HOW_MANY_TIMES_TO_SAY_HELLO 10000
- 
+void say_three_hi()
+
+
 int main(int argv,const char *arc[])
 {         
      int j;
-     for(j = 0; j <= HOW_MANY_TIMES_TO_SAY_HELLO ; j ++) 
-     {
-         
-     }
+     say_three_hi();
      for(i = 0; i <= HOW_MANY_TIMES_TO_SAY_HELLO ; i ++)
      { 
           printf("hello everyone ! is %d\n",i);
      } 
      return 0;
+}
+
+void say_three_hi()
+{
+    int j;
+    for(j = 0; j <= HOW_MANY_TIMES_TO_SAY_HELLO ; j ++)
+    {
+        printf("hello everyone ! is %d\n",j)
+    }
 }
