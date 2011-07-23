a6d849f50d12fb0386f6087bbb9a3591dd0c1df4
kehaohao <534529319@qq.com>
2011-07-23 10:36:26 +0800

    cowsay

--- hello.c
+++ hello.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
-#define HOW_MANY_TIMES_TO_SAY_HELLO 10000 
+#define HOW_MANY_TIMES_TO_SAY_HELLO 10000
+ 
 int main(int argv,const char *arc[])
-{
-     int i;                                         
+{         
+     int j;
+     for(j = 0; j <= HOW_MANY_TIMES_TO_SAY_HELLO ; j ++) 
+     {
+         
+     }
      for(i = 0; i <= HOW_MANY_TIMES_TO_SAY_HELLO ; i ++)
      { 
           printf("hello everyone ! is %d\n",i);
